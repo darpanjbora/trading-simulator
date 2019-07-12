d5c17757d42e63e109df39a21788e85115bdfd13
Darpan Jyoti Bora <darpanjyoti.bora@gmail.com>
2019-07-12 10:47:50 +0530

    Boilerplate commit

---
+++ FX-Trading-Simulator/src/main/java/com/tradingsimulator/FXTradingSimulator/FxTradingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.tradingsimulator.FXTradingSimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxTradingSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxTradingSimulatorApplication.class, args);
+	}
+
+}
